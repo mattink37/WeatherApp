9df3cf9fea6c1546f79dab4f2f76c5e4db0c5b95
Matt Inkeles <mattinkeles37@gmail.com>
2023-02-19 22:31:03 -0500

    theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,29 @@
-import { getCurrentWeather } from "../businessLogic/appRequest";
+import { Grid, ThemeProvider, colors, createTheme } from '@mui/material';
+import WeatherCard from './weatherCard';
+import { blue, blueGrey } from '@mui/material/colors';
+
+export const theme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: blueGrey,
+  },
+});
 
 function App() {
-  getCurrentWeather("Atlanta")?.then((data) => console.log(data));
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        justifyContent={'center'}
+        display={'flex'}
+        height={'100vh'}
+        alignContent={'center'}
+        sx={{ backgroundColor: theme.palette.secondary.dark }}
+      >
+        <WeatherCard />
+      </Grid>
+    </ThemeProvider>
+  );
 }
 
 export default App;
